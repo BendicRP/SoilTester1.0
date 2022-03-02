61d5002bdbb446d519c3d2ef6c9536ec3de04584
BendicRP <benrpacia@gmail.com>
2022-03-02 13:18:40 -0500

    made a random test file Please enter the commit message for your changes. Lines starting

---
+++ SoilTestControl/test_control.py
@@ -0,0 +1,12 @@
+import PiMotor
+import time
+import RPi.GPIO as GPIO
+
+#To drive all motors together
+motorAll = PiMotor.LinkedMotors(m1,m2,m3,m4)
+
+#Names for Individual Arrows
+ab = PiMotor.Arrow(1)
+al = PiMotor.Arrow(2)
+af = PiMotor.Arrow(3) 
+ar = PiMotor.Arrow(4)
