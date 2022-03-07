4658461a27cbd78f910591269de4a0c8daf97dfe
BendicRP <benrpacia@gmail.com>
2022-03-07 10:47:14 -0500

    Created QR camera test code

---
+++ SoilTestControl/QRCameraTest.py
@@ -0,0 +1,18 @@
+from picamera import picamera
+from time import sleep
+from PIL import Image
+import zbarlight
+
+camera = PiCamera()
+while True:
+    camera.start_preview()
+    sleep(5)
+    camera.capture('/home/pi/Desktop/qr.png')
+    camera.stop_preview()
+    file_path = '/home/pi/Desktop/qr.png'
+    with open(file_path, 'rb') as image_file:
+        image = Image.open(image_file)
+        image.load()
+    sleep(5)
+    codes = zbarlight.scan_codes{['qrcode'], image}
+    print('QR codes: %s' % codes)
